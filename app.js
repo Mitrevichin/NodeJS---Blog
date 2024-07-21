bd6da1a6e4ccad4f4b4c404fcf64829df8957a53
Petar Mitrevichin <pmitrevichin@gmail.com>
2024-07-21 18:45:46 +0300

    Login, Auth added

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const hbs = require('express-handlebars');
+const mongoStore = require('connect-mongo');
 const mainRouter = require('./server/routes/main');
 const adminRouter = require('./server/routes/admin');
 const connectDB = require('./server/config/db');
+const MongoStore = require('connect-mongo');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,9 +15,20 @@ const PORT = process.env.PORT || 5000;
 // Connect to DB
 connectDB();
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(cookieParser());
+
+app.use(session({
+    secret: 'keybord cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    }),
+    // cookie: {maxAge: new Date( Date.now() + (36000000) )}
+}));
 
 // Templating engine configuration (hbs)
 app.engine('hbs', hbs.create({
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const Post = require('../models/Post');
 const User = require('../models/User');
 const router = express.Router();
 
+const jwtSecret = process.env.JWT_SECRET;
+
 const adminLayout = '../layouts/admin';
+
+// Login Check Middleware
+const authMiddleware = (req, res, next) => {
+    const token = req.cookies.token;
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, jwtSecret);
+        req.userId = decoded.userId;
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Unauthorized' });
+    }
+}
 
 // Admin - Login Page
 router.get('/admin', async (req, res) => {
@@ -13,7 +34,6 @@ router.get('/admin', async (req, res) => {
             description: 'Simple blog with NodeJS, Express and MongoDB'
         }
 
-
         res.render('admin/index', { locals, layout: adminLayout });
 
     } catch (err) {
@@ -21,6 +41,58 @@ router.get('/admin', async (req, res) => {
         res.status(500).send('Server error')
     }
 
+});
+
+// Admin - Check Login
+router.post('/admin', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid credentials.' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid credentials.' });
+        }
+
+        const token = jwt.sign({ userdId: user._id }, jwtSecret);
+        res.cookie('token', token, { httpOnly: true });
+
+        res.redirect('/dashboard');
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Server error')
+    }
+
+});
+
+// Admin - Register
+router.post('/register', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        try {
+            const user = await User.create({ username, password: hashedPassword });
+            res.status(201).json({ message: 'User created', user });
+        } catch (err) {
+            if (err.code === 11000) {
+                res.status(409).json({ message: 'User already in use' });
+            }
+        }
+
+    } catch (err) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
+
+});
+
+// Admin - Dashboard
+router.get('/dashboard', authMiddleware, async (req, res) => {
+    res.render('admin/dashboard');
 });
 
 module.exports = router;
